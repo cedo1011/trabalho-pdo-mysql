2da78392ccaa4ac3150731287650d96a192c8f0a
cedo1011 <carloseduardodeoliveira11@gmail.com>
2021-09-17 10:56:45 +0000

    at bd

--- vendor/assets/sql/create_database.sql
+++ vendor/assets/sql/create_database.sql
@@ -1,17 +1,16 @@
+
 CREATE DATABASE biblioteca;
 
-USE bilioteca;
+    USE biblioteca;
 
-CREATE TABLE generos(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(255) NOT NULL
+    CREATE TABLE generos(
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        nome VARCHAR (255) NOT NULL
+    );
 
-)
-
-CREATE TABLE livros(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    titulo VARCHAR(255) NOT NULL, 
-    id_genero INT NOT NULL,
-    FOREIGN KEY (id genero) REFERENCES generos(id)
-);
-
+    CREATE TABLE livros(
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        titulo VARCHAR(255) NOT NULL,
+        id_genero INT NOT NULL,
+        FOREIGN KEY (id_genero) REFERENCES generos(id)
+    );
--- vendor/assets/sql/dados_iniciais.sql
+++ vendor/assets/sql/dados_iniciais.sql
@@ -1,11 +1,12 @@
-INSERT INTO generos(id, nome)VALUES
-(1, 'Alta Fantasia'),
-(2, 'policial'),
-(3, 'Drama'),
-(4, 'ficção Científica');
 
-INSERT INTO geneors(id, titulo, id_genero) VALUES
-(1, 'Assassinato no expresso do Oriente', 2),
-(2, 'A sociedade do anel', 1 ),
-(3, 'Como eu era antes de você', 3)
-(4, 'O guia do mochileiro das galáxias', 4)
+INSERT INTO generos(id, nome) VALUES
+          (1, 'Alta Fantasia'),
+          (2, 'Policial'),
+          (3, 'Drama'),
+          (4, 'Ficção Científica');
+
+    INSERT INTO livros(id, titulo, id_genero) VALUES
+        (1, 'Assassinato no expresso do Oriente', 2),
+        (2, 'A sociedade do Anel', 1),
+        (3, 'Como eu era antes de você',3),
+        (4, 'O Guia do Mochileiro das Galaxias', 4);

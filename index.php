0755d16ab7fd79fb8569197eb2a3ac8deffc182b
cedo1011 <carloseduardodeoliveira11@gmail.com>
2021-09-17 10:56:38 +0000

    atualização index

--- index.php
+++ index.php
@@ -1,24 +1,37 @@
 <?php
-require_once './vendedor/autoload.php';
+require_once './vendor/autoload.php';
 use PDO;
 
-$bd = new PDO('msql:host=localhost;dbname=biblioteca', 'root','');
+$bd = new PDO ('mysql:host=localhost;dbname=biblioteca', 'root', '');
+
 $comando = $bd->prepare('SELECT * FROM generos');
 $comando->execute();
-$genero - $comando->fetchall(PDO::FETCH_ASSOC);
+$generos = $comando->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
-<!DOCTYPE html>
-<html lang-"pt-br">
+
+<!Doctype html>
+<html lang="pt-br">
     <head>
-        <meta charset="ulf-8">
-        <title>biblioteca</title>
-</head>
-<body>
-    <table>
-        <tr>
-            <th>td</th>
-            <th>nome</th>
-</tr>
-<?php foreach($generos as $g): ?>
-        
+        <meta charset="utf-8">
+        <title>Biblioteca</title>
+    </head>
+    <body>
+        <table>
+            <tr>
+                <th>Id</th>
+                <th>Nome</th>
+            </tr>
+            <?php foreach($generos as $g): ?>
+                <tr>
+                    <td><?= $g['id']?></td>
+                    <td><?= $g['nome']?></td>
+                </tr>
+            <?php endforeach ?>
+        </table>
+    </body>
+</html>
+
+
+
+

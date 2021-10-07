f01945006c78d8c1ba25cfbc130c2a028ca165e9
cedo1011 <carloseduardodeoliveira11@gmail.com>
2021-10-07 17:07:21 +0000

    Correção

--- update.php
+++ update.php
@@ -8,7 +8,7 @@ $bd = new MySQLConnection();
 $genero = null;
 
 if($_SERVER['REQUEST_METHOD'] == 'GET') {
-    $comando = $bd->prepare('SELECT * FROM genero WHERE id = :id');
+    $comando = $bd->prepare('SELECT * FROM generos WHERE id = :id');
     $comando->execute([':id' => $_GET['id']]);
 
     $genero = $comando->fecht(PDO::FETCH_ASSOC);
@@ -27,14 +27,17 @@ if($_SERVER['REQUEST_METHOD'] == 'GET') {
 <head>
     <meta charset="utf-8">
     <title>Editar Gênero</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
 </head>
 <body>
+    <main class="container">
     <h1>Editar Gênero</h1>
     <form action="update.php" method="post">
-        <input type="hidden" name="id" value="<?= $genero['id'] ?>"
+        <input type="hidden" name="id" value="<?= $genero['id'] ?>" />
         <label for="nome">Nome do Gênero</label>
-        <input type="text" required name="nome" value="<?= $genero['nome'] ?>"
+        <input type="text" name="nome" value="<?= $genero['nome'] ?>" />
         <button type="submit">Salvar</button>
 </form>
+</main>
 </body>
 </html>
